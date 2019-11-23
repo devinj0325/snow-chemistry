439ea4103c7cd05d9937d8a32727d28cc195f8b9
Carolynew <carolyn.e.west@gmail.com>
2019-11-23 11:56:48 -0500

    Updates to py and csv

--- load.py
+++ load.py
@@ -3,7 +3,7 @@ import pandas as pd
 from sqlalchemy import create_engine
 
 # Path to Snow CSV file
-snow_data_to_load = "data/data_1993_2018.csv"
+snow_data_to_load = "data/data_1993_2018_fixed.csv"
 
 # Store Snow CSV into DataFrame
 snow_data = pd.read_csv(snow_data_to_load)
@@ -14,20 +14,21 @@ filtered_snow_data = snow_data.drop(columns=['SiteNumber', 'SampleDate', 'Sample
 'Hg (ng/L) Roth 1994', 'Hg (ng/L) USEPA 1631', 'd34S (per mil)', 'SWE (cm)', 'Ionic Balance (%)'])
 
 # Determine which stations have 20 or more observations; drop the stations that have fewer than 20 observations
-station_count = pd.DataFrame(station_count)
-station_count.reset_index(inplace=True)
-station_count.rename(columns={"index": "StationName", "StationName": "Station_Count"}, inplace=True)
-filtered_snow_data = filtered_snow_data.merge(station_count, on = 'StationName', how = 'left')
-filtered_snow_data.dropna(inplace=True)
+station_count = filtered_snow_data['StationName'].value_counts()
+station_count = station_count[station_count >= 20]
 
-# Renaming columns for readability
-filtered_snow_data.rename(columns={"H (ueq/L)": "Hydrogen", "Ca (ueq/L)": "Calcium", 
-"Mg (ueq/L)": "Magnesium", "Na (ueq/L)": "Sodium", "K (ueq/L)": "Potassium", 
-"NH4 (ueq/L)": "Ammonium", "Cl (ueq/L)": "Chloride", "SO4 (ueq/L)": "Sulfate", 
-"NO3 (ueq/L)": "Nitrate", "DOC (mg/L)": "Dissolved_organic_carbon", "Snow Depth (cm)": "Snow_depth"}, inplace=True)
+station_count_df = pd.DataFrame(station_count)
+station_count_df.rename(columns={"index": "StationName", "StationName": "StationCount"}, inplace=True)
+filtered_snow_data = filtered_snow_data.join(other=station_count_df, on="StationName", how="inner")
 
 # Removing Station Count column
-final_snow_data = filtered_snow_data.drop(columns=['Station_Count'])
+filtered_snow_data = filtered_snow_data.drop(columns="StationCount")
+
+# Renaming columns for readability
+filtered_snow_data.rename(columns={"H (ueq/L)": "Hydrogen", "Ca (ueq/L)": "Calcium", "Mg (ueq/L)": "Magnesium", 
+    "Na (ueq/L)": "Sodium", "K (ueq/L)": "Potassium", "NH4 (ueq/L)": "Ammonium", 
+    "Cl (ueq/L)": "Chloride", "SO4 (ueq/L)": "Sulfate", "NO3 (ueq/L)": "Nitrate", 
+    "DOC (mg/L)": "Dissolved_organic_carbon", "Snow Depth (cm)": "Snow_depth"}, inplace=True)
 
 
 # Path to Stations CSV
@@ -41,5 +42,5 @@ engine = create_engine('postgresql://postgres:postgres@localhost:5432/snow_chemi
 
 
 # # Use Pandas To Load CSV converted DataFrame into database
-final_snow_data.to_sql(name='snow_data', con=engine, if_exists='replace', index=True, index_label='id')
-stations.to_sql()
+filtered_snow_data.to_sql(name='snow_data', con=engine, if_exists='replace', index=True, index_label='id')
+stations.to_sql(name='stations', con=engine, if_exists='replace', index=True, index_label='id')
