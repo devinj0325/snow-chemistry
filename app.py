7c60af06cedad339ad6ab00a47494dd66cd2f9a9
Chelsea Scheetz <clscheetz@gmail.com>
2019-11-24 14:20:29 -0500

    attempts to get FLASK working

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ stations = Base.classes.stations
 @app.route("/")
 
 def index():
-  render_template("index.html")
+  render_template("templates/index.html")
 
 @app.route("/snow")
 def snow_chemistry():
