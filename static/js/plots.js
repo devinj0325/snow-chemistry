9e036a85f9edc4c6feffd41e588b410bffa49260
Chelsea Scheetz <clscheetz@gmail.com>
2019-12-01 19:04:43 -0500

    set default chart values

--- static/js/plots.js
+++ static/js/plots.js
@@ -27,30 +27,30 @@ svg.append('text')
   .classed('chart-label', true);
 
 svg.append('text')
-  .attr('x', 0)
-  .attr('y', svgHeight / 2)
-  .text('Depth (cm)')
-  .classed('chart-label', true);
-
+.attr('x', 0)
+.attr('y', svgHeight / 2)
+.text('Depth (cm)')
+.classed('chart-label', true);
+  
 // Create variable to store all snow data, globally 
 let snowData = [];
 
 let renderChart = function(filteredData) {
-
+  
   // Clear chartGroup on svg for rerender
   chartGroup.selectAll("*").remove();
- 
+  
   // Configure a scale
   // d3.extent returns an array containing the min and max values for the property specified
   var xLinearScale = d3.scaleLinear()
-    .domain(d3.extent(filteredData, data => data['WaterYear']))
-    .range([0, chartWidth]);
-
-    // Configure a linear scale with a range 
+  .domain(d3.extent(filteredData, data => data['WaterYear']))
+  .range([0, chartWidth]);
+  
+  // Configure a linear scale with a range 
   var yLinearScale = d3.scaleLinear()
-    .domain([0, d3.max(filteredData, data => data['Snow_depth'])])
-    .range([chartHeight, 0]);
-
+  .domain([0, d3.max(filteredData, data => data['Snow_depth'])])
+  .range([chartHeight, 0]);
+  
   // Create two new functions passing the scales in as arguments
   // These will be used to create the chart's axes
   var bottomAxis = d3.axisBottom(xLinearScale).tickFormat(d3.format("d"));
@@ -58,51 +58,29 @@ let renderChart = function(filteredData) {
   
   // Configure a line function which will plot the x and y coordinates using our scales
   var drawLine = d3.line()
-    .x(data => xLinearScale(data['WaterYear']))
-    .y(data => yLinearScale(data['Snow_depth']));
+  .x(data => xLinearScale(data['WaterYear']))
+  .y(data => yLinearScale(data['Snow_depth']));
   
-    // Append an SVG path and plot its points using the line function
+  // Append an SVG path and plot its points using the line function
   // The drawLine function returns the instructions for creating the line for forceData
   
   chartGroup.append("path")
-    .attr("d", drawLine(filteredData))
-    .classed("line", true);
+  .attr("d", drawLine(filteredData))
+  .classed("line", true);
   
-    // Append an SVG group element to the chartGroup, create the left axis inside of it
+  // Append an SVG group element to the chartGroup, create the left axis inside of it
   chartGroup.append("g")
-    .classed("axis", true)
-    .call(leftAxis);
+  .classed("axis", true)
+  .call(leftAxis);
   
-    // Append an SVG group element to the chartGroup, create the bottom axis inside of it
+  // Append an SVG group element to the chartGroup, create the bottom axis inside of it
   // Translate the bottom axis to the bottom of the page
   chartGroup.append("g")
-    .classed("axis", true)
-    .attr("transform", `translate(0, ${chartHeight})`)
-    .call(bottomAxis);
+  .classed("axis", true)
+  .attr("transform", `translate(0, ${chartHeight})`)
+  .call(bottomAxis);
 };
-
-// Use D3 to pull snow depth data from flask app
-d3.json('/snow').then(function(jsonData) {
-
-  snowData = jsonData;
-});
-
-// Use D3 tp pull stations from flask app
-
-d3.json('/stations').then(function(stationData) {
-
-  // Select element in html
-  let selStation = d3.select('#selStation');
-
-  // Populate options for each station
-  stationData.forEach(station => {
-    selStation.append('option')
-      .attr('value', station['StationName'])
-      .text(station['StationName']);
-  });
-
-});
-
+  
 // Function for updating chart based on station name
 let updateStation = function(stationName) {
   let filteredData = snowData
@@ -111,3 +89,30 @@ let updateStation = function(stationName) {
   // Renders chart
   renderChart(filteredData);
 };
+
+// Use D3 to pull snow depth data from flask app
+d3.json('/snow').then(function(jsonData) {
+  
+  snowData = jsonData;
+
+  // Use D3 tp pull stations from flask app
+  d3.json('/stations').then(function(stationData) {
+    
+    // Select element in html
+    let selStation = d3.select('#selStation');
+
+    // Populate options for each station
+    stationData.forEach(station => {
+      selStation.append('option')
+        .attr('value', station['StationName'])
+        .text(station['StationName']);
+
+      });
+      
+    let defaultStationName = stationData[0]['StationName'];
+    selStation.node().value = defaultStationName;
+    updateStation(defaultStationName);
+  });
+});
+  
+
